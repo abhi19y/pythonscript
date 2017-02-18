29c6a4f9f7c014dd918c81f95fa0f525380eb2bd
abhi19y <abhiy.front@gmail.com>
2017-02-18 20:17:33 +0530

    config file added

---
+++ config.py
@@ -0,0 +1,17 @@
+config = {
+
+    'ec2_region_name': 'us-east-1',
+    'ec2_region_endpoint': 'ec2.us-east-1.amazonaws.com',
+    'aws_access_key' : '<aws_access_key>',
+    'aws_secret_key' : '<aws_secret_key>',
+    'owner' : 'owner_id',
+    'qa_vol_id' : 'vol-id',
+    'uat_vol_id' : 'vol-d',
+    'qa_days_old' : '2',
+    'uat_days_old' : '2',
+    'tag_name': 'Name',
+    'tag_value': 'qashare-',
+    'tag_name': 'Name',
+    'tag_value1': 'uatshare-',
+
+}
